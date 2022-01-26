d61606910cfd4fd1e48b29b372bec40c2b4e8acc
Ikhtiyor <standardmail77@gmail.com>
2022-01-27 03:17:13 +0500

    Initial Commit

---
+++ MokaCodingTDD/MokaCodingTDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  MokaCodingTDDApp.swift
+//  MokaCodingTDD
+//
+//  Created by Ikhtiyor Nurmatov on 27/01/22.
+//
+
+import SwiftUI
+
+@main
+struct MokaCodingTDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
